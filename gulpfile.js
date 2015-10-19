0a79c8706b5f76ae23f427c2929e28262bd151aa
smallg <gwpxjb@163.com>
2015-10-19 10:44:03 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,8 @@ var paths = {
     css: 'src/css/*.css'
 };
 
-gulp.task('minify-js', function(){
+gulp.task('minify-js', function () {
     return gulp.src(paths.scripts)
-        .pipe(jshint('.jshintrc'))
         .pipe(concat('third.concat.js'))
         .pipe(gulp.dest('dist'))
         .pipe(rename('third.min.js'))
@@ -22,7 +21,7 @@ gulp.task('minify-js', function(){
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify-css', function() {
+gulp.task('minify-css', function () {
     return gulp.src(paths.css)
         .pipe(concat('third.concat.css'))
         .pipe(gulp.dest('dist'))
@@ -31,9 +30,34 @@ gulp.task('minify-css', function() {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', function () {
+gulp.task('only-minify-js', ['clean-js'], function () {
+    return gulp.src(paths.scripts)
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('only-minify-js'));
+});
+gulp.task('only-minify-css', ['clean-css'], function () {
+    return gulp.src(paths.css)
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('only-minify-css'));
+});
+
+gulp.task('clean-js', function () {
+    return gulp.src('only-minify-js', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('clean-css', function () {
+    return gulp.src('only-minify-css', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('clean-dist', function () {
     return gulp.src('dist', {read: false})
         .pipe(clean());
 });
 
-gulp.task('build', ['minify-js', 'minify-css']);
+gulp.task('build', ['clean-dist', 'minify-js', 'minify-css']);
+gulp.task('only', ['only-minify-js', 'only-minify-css']);
+gulp.task('clean', ['clean-dist', 'clean-js', 'clean-css']);
